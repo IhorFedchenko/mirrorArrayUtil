a6447996c38e542f0cf961563e470889fb5f5485
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-11-22 23:39:03 +0200

    prototype the loop for swap is completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        int length = 4;
+        int deep = 1;
+        int mirrorDeep = length - deep;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - deep; j++) {
+                System.out.print("oldY: "+i+" "+"oldX: "+j+" "+" newY: "+(mirrorDeep-j)+" newX: "+(length-deep)+"\n");
+            }
+            deep++;
 
+        }
     }
 }
--- src/test/java/MirrorTest.java
+++ src/test/java/MirrorTest.java
@@ -21,7 +21,7 @@ public class MirrorTest {
     }
 
     @Test
-    public void upDown3() {
+    public void upDown_When_Size_is_3() {
         Mirror mirror = new Mirror();
         int[][] array = new int[3][3];
         array[0] = new int[]{1, 2, 3};
@@ -32,7 +32,7 @@ public class MirrorTest {
     }
 
     @Test
-    public void upDown4() {
+    public void upDown_When_Size_is_4() {
         Mirror mirror = new Mirror();
         int[][] array = new int[4][4];
         array[0] = new int[]{1, 2, 3, 4};
@@ -41,13 +41,13 @@ public class MirrorTest {
         array[3] = new int[]{13, 14, 15, 16};
         mirror.upDown(array);
         assertEquals("[13, 14, 15, 16]\n" +
-                              "[9, 10, 11, 12]\n" +
-                              "[5, 6, 7, 8]\n" +
-                              "[1, 2, 3, 4]\n", mirror.toString(array));
+                "[9, 10, 11, 12]\n" +
+                "[5, 6, 7, 8]\n" +
+                "[1, 2, 3, 4]\n", mirror.toString(array));
     }
 
     @Test
-    public void leftRight3() {
+    public void leftRight_When_Size_is_3() {
         Mirror mirror = new Mirror();
         int[][] array = new int[3][3];
         array[0] = new int[]{1, 2, 3};
@@ -56,8 +56,9 @@ public class MirrorTest {
         mirror.leftRight(array);
         assertEquals("[3, 2, 1]\n" + "[6, 5, 4]\n" + "[9, 8, 7]\n", mirror.toString(array));
     }
+
     @Test
-    public void leftRight4() {
+    public void leftRight_When_Size_is_4() {
         Mirror mirror = new Mirror();
         int[][] array = new int[4][4];
         array[0] = new int[]{1, 2, 3, 4};
@@ -66,8 +67,34 @@ public class MirrorTest {
         array[3] = new int[]{13, 14, 15, 16};
         mirror.leftRight(array);
         assertEquals("[4, 3, 2, 1]\n" +
-                              "[8, 7, 6, 5]\n" +
-                              "[12, 11, 10, 9]\n"+
-                              "[16, 15, 14, 13]\n", mirror.toString(array));
+                "[8, 7, 6, 5]\n" +
+                "[12, 11, 10, 9]\n" +
+                "[16, 15, 14, 13]\n", mirror.toString(array));
+    }
+
+    @Test
+    public void d1_When_Size_is_3() {
+        Mirror mirror = new Mirror();
+        int[][] array = new int[3][3];
+        array[0] = new int[]{1, 2, 3};
+        array[1] = new int[]{4, 5, 6};
+        array[2] = new int[]{7, 8, 9};
+        mirror.d1(array);
+        assertEquals("[9, 6, 3]\n" + "[8, 5, 2]\n" + "[7, 4, 1]\n", mirror.toString(array));
+    }
+
+    @Test
+    public void d1_When_Size_is_4() {
+        Mirror mirror = new Mirror();
+        int[][] array = new int[4][4];
+        array[0] = new int[]{1, 2, 3, 4};
+        array[1] = new int[]{5, 6, 7, 8};
+        array[2] = new int[]{9, 10, 11, 12};
+        array[3] = new int[]{13, 14, 15, 16};
+        mirror.d1(array);
+        assertEquals("[16, 12, 8, 4]\n" +
+                "[15, 11, 7, 3]\n" +
+                "[14, 10, 6, 2]\n" +
+                "[13, 9, 5, 1]\n", mirror.toString(array));
     }
 }
